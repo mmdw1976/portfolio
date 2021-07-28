00e52f1b79c2456ede9a26031c294183169064cf
Mario <mmdw1976@gmail.com>
2021-07-28 17:03:33 +0200

    added ProjectList and ProjectItem and set basic styling

---
+++ src/components/projects/ProjectItem.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react'
+
+const ProjectItem = () => {
+    return ( 
+        <Fragment>
+            <div className="snippet">
+                <div className="snippet-bg"></div>
+            </div>
+            <div className="card-content">
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo magnam qui soluta, reprehenderit dolore atque esse, vitae maxime libero, dolorum officia iusto tempore est mollitia dicta. Aut officiis ex vero, enim aspernatur nihil aliquam in est, laboriosam consectetur, eos nemo!
+                </p>
+            </div>
+        </Fragment>
+     );
+}
+ 
+export default ProjectItem;
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ProjectItem from './ProjectItem';
+
+const ProjectList = () => {
+    return ( 
+        <div className="card">
+            <ProjectItem />
+        </div>
+     );
+}
+ 
+export default ProjectList;
