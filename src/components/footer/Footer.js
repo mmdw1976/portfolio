a2f4680d8aa3f0b6296b59ea681af38b8db29c5b
Mario <mmdw1976@gmail.com>
2021-08-01 00:02:35 +0200

    Made some adjustments for mobile layout on home.css, projectlist.css and footer.css

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProjectContext } from '../../contexts/ProjectsContext';
 
 import './Footer.css';
 
 const Footer = () => {
-    return ( 
-        <footer>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-        </footer>
-     );
-}
- 
+  const { state } = useContext(ProjectContext);
+  return (
+    <footer>
+      <div className='box'></div>
+      <div className='box'>
+        <h2>Projecten</h2>
+        <table>
+          <tbody>
+            {state.map((project) => {
+              return (
+                <tr key={project.id}>
+                  <td>{project.name}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className='box'></div>
+    </footer>
+  );
+};
+
 export default Footer;
